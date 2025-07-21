52e0e3219e5255a1a90ff23693b9346edcc99cb2
Stephan Koster <skoster@enclustra.com>
2025-07-21 11:42:07 +0200

    added n_strands parameter, added gif creation, extracted image create to a function

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def gen_circles(polars_c: np.ndarray, feature:int, ts:int) -> np.ndarray:
     return circles_pixels
 
 
-def gen_spirals(polars_s: np.ndarray, feature:int, ts:float) ->np.ndarray:
+def gen_spirals(polars_s: np.ndarray, feature:int, ts:float, num_strands:int=1) ->np.ndarray:
     assert (0<=ts<1)
     shape=(polars_s.shape[0], polars_s.shape[1])
     spiral_pixels = np.zeros(shape)
@@ -26,7 +26,7 @@ def gen_spirals(polars_s: np.ndarray, feature:int, ts:float) ->np.ndarray:
         for j in range(shape[1]):
             pol=polars_s[i,j]
             sval = pol[0]
-            sval += (pol[1]/(2 * np.pi))*feature
+            sval += (pol[1]*num_strands/(2 * np.pi))*feature
             sval += ts*feature
             sval = sval%feature/feature
             spiral_pixels[i, j] = sval * 255
@@ -40,16 +40,27 @@ def getPolars(shape: np.shape) -> np.ndarray:
             polars[i, j,] = cart2pol(i - midpoint[0], j - midpoint[1])
     return polars
 
-if __name__ == '__main__':
-    # pixels = genCircles((1000, 1000))
-    polars=getPolars((3000,3000))
-    nSteps=16
-    for t in range(0,nSteps):
-        pixels = gen_spirals(polars, feature=60, ts=t / nSteps)
-
+def func_to_gif(func, shape:np.shape, steps: int):
+    polars = getPolars((shape[0]*3, shape[1]*3))
+    n_steps = 16
+    image_array = []
+    for t in range(0, n_steps):
+        pixels = func(polars=polars, feature=60, ts=t / n_steps)
         img = Image.fromarray(pixels)
-        smallImg = img.resize((1000, 1000), Image.Resampling.LANCZOS)
-        smallImg = smallImg.convert('RGB')
-        smallImg.save(f"spiral{t}.bmp")
-        #smallImg.show()
+        small_img = img.resize(shape, Image.Resampling.LANCZOS)
+        small_img = small_img.convert('RGB')
+        small_img.save(f"spiral{t}.bmp")
+        image_array.append(small_img)
+        # smallImg.show()
         print(t)
+    combined = image_array[0]
+    combined.save("spirals.gif", save_all=True, append_images=image_array[1:], duration=100, loop=0)
+
+    return
+
+def main():
+    func= lambda polars, feature, ts:gen_spirals(polars, feature, ts, num_strands=3)
+    func_to_gif(func=func, shape=(1000,1000), steps=16)
+
+if __name__ == '__main__':
+    main()
